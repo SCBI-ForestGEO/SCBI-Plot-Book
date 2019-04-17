b38b055453a5531d306f68391d91fa3ae7e1e01c
mcgregorian1 <mcgregori@si.edu>
2019-04-17 12:31:16 -0400

    Restructured code

--- R Scripts/chapter_3/US_range_map.R
+++ R Scripts/chapter_3/US_range_map.R
@@ -1,97 +1,17 @@
-# creating maps for scbi plotbook
-
-#1 easy range map code for book (plot and regional) ####
-#1a for scbi plot ####
-library(leaflet)
-library(maps)
-library(htmlwidgets)
-library(maptools)
-
-dendro <- read.csv("maps_and_figures/dendro_cored_full[sample].csv")
-
-dendro$tag <- as.character(dendro$tag)
-
-map <- leaflet(data = dendro) %>%
-  # setView(-72.14600, 43.82977, zoom = 8) %>% 
-  addProviderTiles("CartoDB.Positron", group = "Map") %>%
-  addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>% 
-  addProviderTiles("Esri.WorldShadedRelief", group = "Relief") %>%
-  addMarkers(lng=~lon, lat=~lat, label = ~tag, group = "Trees") %>% 
-  addPolygons(data=dendro, groups="Trees",weight=2, fillOpacity = 0) %>%
-  addScaleBar(position = "bottomleft") %>%
-  addLayersControl(
-    baseGroups = c("Map", "Topo", "Relief"),
-    overlayGroups = c("Trees", "States"),
-    options = layersControlOptions(collapsed = FALSE)
-  )
-
-invisible(print(map))
-saveWidget(map, file="maps_and_figures/dendro.html", selfcontained=TRUE)
+######################################################
+# Purpose: Build US range maps of species present in ForestGEO plot
+# Developed by: Ian McGregor - mcgregori@si.edu
+# R version 3.5.2 
+######################################################
+library(RCurl) #1a
+library(data.table) #1a
+library(leaflet) #1b
+library(maps) #1b
+library(htmlwidgets) #1b
+library(maptools) #1b
 
 
-#1b for sp range maps ####
-library(leaflet)
-library(maps)
-library(htmlwidgets)
-library(maptools)
-
-library(rgdal)
-
-litu <- readOGR("maps_and_figures/liriodendron_tulipifera_range.shp")
-
-library(sp)
-##the below line of code defines the projection as being WGS1984
-#pj84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-
-map <- leaflet() %>%
-  # setView(-72.14600, 43.82977, zoom = 8) %>% 
-  addProviderTiles("CartoDB.Positron", group = "Map") %>%
-  addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>% 
-  addProviderTiles("Esri.WorldShadedRelief", group = "Relief") %>%
-  addPolygons(data=litu, weight=2, fill=TRUE, fillColor="blue", fillOpacity = 0.25) %>%
-  #addPolygons(data=quru, weight=1, color="#115F", fillOpacity = 0 [opacity is between 0 and 1]) %>%
-  addScaleBar(position = "bottomleft") %>%
-  addLayersControl(
-    baseGroups = c("Map", "Topo", "Relief"),
-    options = layersControlOptions(collapsed = FALSE)
-  )
-
-
-q <- leaflet() %>%
-  addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>%
-  addScaleBar(position = "bottomleft")
-
-invisible(print(map))
-
-saveWidget(map, file="maps_and_figures/litumap.html", selfcontained=TRUE)
-
-#you can have multiple addPolygons lines, with either reading in different shapefiles or reading in actual shapes. Be sure to use ?addPolygons to see all the options (as in, fill color, popup, shape, etc).
-
-
-#2 convert kml to shapefile ####
-#1 first you have to rename the kmz as a zip (bc it's a zipped kml), then unzip it.
-fnm <- c("maps_and_figures/Quercus_prinus_final.dynglobcurrent3.elev.30000.kmz")
-
-quprkml <- unzip(zipfile = "maps_and_figures/Quercus_prinus_final.dynglobcurrent3.elev.30000.kmz", exdir = "maps_and_figures")
-
-#2 then you open the kml file in R and convert it to a shapefile
-
-## this can easily be done with readOGR and writeOGR.
-## however, the layer attribute of the function is specified as what the layer is called within the kml file, which is NOT the same as the file name. The only way to find the layer name is by opening the kml file as a txt and finding what the polygon (in this case) is labeled as. See also (ogrListLayers and ogrInfo).
-
-## in our case, the maps downloaded from ForeCASTS project don't have standard kmz format. A kmz = a zip kml file. These kmz files only have a basic kml file with a png attached showing the sp range. Current R packages can't work with this (#layers are defined as 0).
-
-readOGR("maps_and_figures",layer="Quercus_prinus_final.dynglobcurrent3.elev.30000.kml")
-writeOGR("maps_and_figures/quercus_prinus_range.shp",driver="ESRI Shapefile",layer=output.shp)
-
-
-ogrListLayers("Quercus_prinus_final.dynglobcurrent3.elev.30000.kml")
-
-
-
-#3a determine the sp and shp files available from BIEN ####
-library(RCurl)
-
+#1a determine the species and .shp files available from BIEN ####
 ##read in sp list used for book
 fullsp <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/species_lists/Tree%20ecology/SCBI_ForestGEO_sp_ecology.csv"))
 
@@ -107,7 +27,6 @@ subsp$sp <- gsub("Sambucus_canadensis var. canadensis", "Sambucus_canadensis", s
 splist <- c(subsp$sp)
 
 ##read in BIEN website data and make clear what species are on website
-library(data.table)
 test <- fread("http://vegbiendev.nceas.ucsb.edu/bien/data/ranges/shapefiles/")
 colnames(test) <- c("full.string", "align", "breaks")
 test$sp <- sub(".*zip\">", "", test$full.string)
@@ -126,11 +45,7 @@ spfiles <- c(paste0("http://vegbiendev.nceas.ucsb.edu/bien/data/ranges/shapefile
 
 URLs <- spfiles
 
-#3b Call the shapefiles, unzip them, then make html maps for each ####
-library(leaflet)
-library(maps)
-library(htmlwidgets)
-library(maptools)
+#1b Call the shapefiles, unzip them, then make html maps for each ####
 
 ##the following code snippet defines a function whereby shapefile zip files are downloaded into a temp folder and directory.
 ##temp folders are automatically unlinked (deleted) and the working directory is set back to original within every iteration of the function. This means no shapefiles are stored to the drive.
@@ -179,5 +94,65 @@ for (sp in names(y)){
   )
   
 invisible(print(map))
-saveWidget(map, file=paste0("maps_and_figures/range_maps/", sp,"_map.html"), selfcontained=TRUE)
+saveWidget(map, file=paste0("maps_figures_tables/ch_3_US_range_maps/", sp,"_map.html"), selfcontained=TRUE)
 }
+
+
+#2 troubleshooting code ####
+library(leaflet)
+library(maps)
+library(htmlwidgets)
+library(maptools)
+library(rgdal)
+
+##download litu shapefile from BIEN: http://vegbiendev.nceas.ucsb.edu/bien/data/ranges/shapefiles/
+litu <- readOGR("~liriodendron_tulipifera_range.shp")
+
+library(sp)
+##the below line of code defines the projection as being WGS1984
+#pj84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+map <- leaflet() %>%
+  # setView(-72.14600, 43.82977, zoom = 8) %>% 
+  addProviderTiles("CartoDB.Positron", group = "Map") %>%
+  addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>% 
+  addProviderTiles("Esri.WorldShadedRelief", group = "Relief") %>%
+  addPolygons(data=litu, weight=2, fill=TRUE, fillColor="blue", fillOpacity = 0.25) %>%
+  #addPolygons(data=quru, weight=1, color="#115F", fillOpacity = 0 [opacity is between 0 and 1]) %>% #can add multiple ranges to one map
+  addScaleBar(position = "bottomleft") %>%
+  addLayersControl(
+    baseGroups = c("Map", "Topo", "Relief"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+
+q <- leaflet() %>%
+  addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>%
+  addScaleBar(position = "bottomleft")
+
+invisible(print(map))
+
+saveWidget(map, file="~litumap.html", selfcontained=TRUE)
+
+#you can have multiple addPolygons lines, with either reading in different shapefiles or reading in actual shapes. Be sure to use ?addPolygons to see all the options (as in, fill color, popup, shape, etc).
+
+#2a convert kml to shapefile ####
+##it is possible to convert kml/kmz files to shapefiles (if a source like BIEN isn't available). The below code is the attempt to use this, and it is being preserved for posterity.
+
+##first you have to rename the kmz as a zip (bc it's a zipped kml), then unzip it.
+fnm <- c("~Quercus_prinus_final.dynglobcurrent3.elev.30000.kmz")
+
+quprkml <- unzip(zipfile = "~Quercus_prinus_final.dynglobcurrent3.elev.30000.kmz", exdir = "folder/")
+
+# then you open the kml file in R and convert it to a shapefile
+## this can easily be done with readOGR and writeOGR.
+## however, the layer attribute of the function is specified as what the layer is called within the kml file, which is NOT the same as the file name. The only way to find the layer name is by opening the kml file as a txt and finding what the polygon (in this case) is labeled as. See also (ogrListLayers and ogrInfo).
+
+## in our case, the maps downloaded from ForeCASTS project don't have standard kmz format. A kmz = a zip kml file. These kmz files only have a basic kml file with a png attached showing the sp range. Current R packages can't work with this (#layers are defined as 0).
+
+readOGR("maps_and_figures",layer="Quercus_prinus_final.dynglobcurrent3.elev.30000.kml")
+writeOGR("maps_and_figures/quercus_prinus_range.shp",driver="ESRI Shapefile",layer=output.shp)
+
+ogrListLayers("Quercus_prinus_final.dynglobcurrent3.elev.30000.kml")
+
+
