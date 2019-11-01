4f8d20f19f868e102f345501120745c2460d7df6
Alyssa Terrell <TerrellA3@si.edu>
2019-11-01 15:32:08 -0400

    Add some code to data table test file

--- R Scripts/chapter_3/data table test.R
+++ R Scripts/chapter_3/data table test.R
@@ -56,3 +56,33 @@ for(sp in species) {
 for (i in seq(along=(all_sp_test))){
   write.csv(all_sp_test[[i]], paste0("C:/Users/terrella3/Dropbox (Smithsonian)/GitHub_Alyssa/SCBI-Plot-Book/R Scripts/test_df/", names(all_sp_test)[[i]], ".csv"), row.names=TRUE)
 }
+
+
+
+
+
+############## THIS CODE IS WORKING
+recensus2018 <- read.csv("V:/SIGEO/2-RECENSUS 2018/DATA/CTFS_Backups/recensus2018.csv", stringsAsFactors = FALSE)
+str(recensus2018)
+table(recensus2018$Codes)
+
+# Will produce a table that includes 'dead' statuses and dbh starting at '0'
+# To get rid of this, limit what dbh measurements and statuses should be included in data
+
+dead <- c("D", "DS", "DC", "DN", "DS;R", "Dc;R")
+recensus_sub <- subset(recensus2018, recensus2018$DBH >= 1 & !(recensus2018$Codes %in% dead))
+
+tapply(recensus_sub$DBH, recensus_sub$Mnemonic, summary, DBH > 0)
+summary.by.sp <- tapply(recensus_sub$DBH, recensus_sub$Mnemonic, function(x)
+  return(round(data.frame(min = min(x), mean = mean(x), max = max(x), n = length(x)), 2)))
+
+summary.by.sp <- data.frame(sp = names(summary.by.sp), do.call(rbind, summary.by.sp), row.names = NULL)
+
+summary.by.sp$newcolumn <- c("New Stems", "Dead Stems")
+
+
+
+
+test <- subset(summary.by.sp, select = c("sp", "min", "max", "n"))
+
+colnames(test) <- c("Species", "Min DBH", "Max DBH", "Total Stems")
