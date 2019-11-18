e24d7eef7499caba98441caf887fd44078add865
Alyssa Terrell <TerrellA3@si.edu>
2019-11-18 10:47:01 -0500

    Render book

--- R Scripts/chapter_3/data table test_new.R
+++ R Scripts/chapter_3/data table test_new.R
@@ -1,18 +1,29 @@
-library(data.table)
-scbi_stem1<- read.csv("C:/Users/TerrellA3/Dropbox (Smithsonian)/GitHub_Alyssa/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.stem1.csv")
-scbi_stem2<- read.csv("C:/Users/TerrellA3/Dropbox (Smithsonian)/GitHub_Alyssa/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.stem2.csv")
-scbi_stem3<- read.csv("C:/Users/TerrellA3/Dropbox (Smithsonian)/GitHub_Alyssa/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.stem3.csv")
+###########################################################
+# Purpose: Create table of all species within the SCBI plot
+# Developed by: Alyssa Terrell - terrella3@si.edu
+# R version 3.6.1 - Created November 2019
+###########################################################
 
-              
+# Call data from local drive
+
+for(f in paste0("scbi.stem", 1:3, ".rdata")) {
+  print(f)
+  url <- paste0("https://raw.github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/", f)
+  download.file(url, f, mode = "wb")
+  load(f)
+  file.remove(f)
+}
+
+# Create structure of data table
 trialmat <- matrix(nrow = 9, ncol = 3)
 colnames(trialmat) <- c("2008", "2013", "2018")
 rownames(trialmat) <- c("Total Stems", "New Stems (recruit rate)", "Dead Stems (mortality rate)", "Min DBH", "Max DBH", "Mean Growth Rate <10cm", "Mean Growth Rate >10cm", "95% Growth Rate <10cm", "95% Growth Rate >10cm")
-#trialmat <- trialmat
+##trialmat <- trialmat
 
 
 trial <- as.data.frame(trialmat)
 
-yearsfiles <- list(scbi_stem1, scbi_stem2, scbi_stem3)
+yearsfiles <- list(scbi.stem1, scbi.stem2, scbi.stem3)
 
 uni <- unique(test$sp)
 
@@ -32,21 +43,21 @@ testnotalive <- subset(x, DFstatus != "alive")
 
 
 
-co <- nrow(libetest)
+count <- nrow(libetest)
 libetest$dbh <- as.numeric(libetest$dbh)
-mi <- min(libetest$dbh)
-ma <- max(libetest$dbh)
+min <- min(libetest$dbh)
+max <- max(libetest$dbh)
 cm10great <- subset(libetest, dbh > 10)
 cm10less <- subset(libetest, dbh < 10)
 greatmean <- mean(cm10great$dbh)
 lessmean <- mean(cm10less$dbh)
-conotalive <- nrow(testnotalive)
+countnotalive <- nrow(testnotalive)
 
 
-trial[1,i] <- co
-trial[3,i] <- conotalive
-trial[4,i] <- mi
-trial[5,i] <- ma
+trial[1,i] <- count
+trial[3,i] <- countnotalive
+trial[4,i] <- min
+trial[5,i] <- max
 trial[6,i] <- lessmean
 trial[7,i] <- greatmean
 
